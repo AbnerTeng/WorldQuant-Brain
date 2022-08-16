844d8b248542c6fb9cad9c4e743c9202149f05f3
AbnerTeng <abnerteng16@gmail.com>
2022-08-16 16:54:44 +0800

    add time sleep

--- test1.py
+++ test1.py
@@ -3,7 +3,10 @@ import email
 import selenium
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+import time
 
+## go to brain website
 options = Options()
 options.add_argument('--disable-notifications')
 
@@ -12,10 +15,11 @@ driver.get('https://platform.worldquantbrain.com/sign-in')
 
 Accept = driver.find_element(By.CLASS_NAME, 'button--primary')
 Accept.click()
+
+time.sleep(1)
 ## driver.close()
 
-# %%
-from selenium.webdriver.common.by import By
+## login
 
 email = driver.find_element(By.ID, 'email')
 password = driver.find_element(By.ID, 'password')
@@ -26,12 +30,16 @@ your_password = 'teng1234'
 email.send_keys(your_email)
 password.send_keys(your_password)
 password.submit()
+
+# %%
 Skip = driver.find_element(By.CLASS_NAME, 'introjs-skipbutton')
 Skip.click()
 
-# %%
+## go to the simulate page
 Simulate = driver.find_element(By.CLASS_NAME, 'header__img--simulate')
 Simulate.click()
+
+time.sleep(2)
 Skip = driver.find_element(By.CLASS_NAME, 'introjs-skipbutton')
 Skip.click()
 # %%
