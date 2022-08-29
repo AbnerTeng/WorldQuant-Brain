099994910c9d5b7bcdedc3e4bfd3b658819a15c9
AbnerTeng <abnerteng16@gmail.com>
2022-08-29 16:45:31 +0800

    renew df

--- df.py
+++ df.py
@@ -6,3 +6,11 @@ file = './price_to_volume.csv'
 df = pd.read_csv(file)
 print(df)
 # %%
+df.to_csv('decay=10.csv')
+# %%
+import pandas as pd
+
+file = './test.csv'
+
+df = pd.read_csv(file)
+print(df)
